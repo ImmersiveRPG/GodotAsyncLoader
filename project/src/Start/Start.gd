df39c489ff834e3ef585d6c3f0c9a7f2a9b4fbe3
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2021-10-28 15:23:30 -0700

    Added button for loading sync or async

--- project/src/Start/Start.gd
+++ project/src/Start/Start.gd
@@ -5,6 +5,32 @@
 extends Control
 
 
-func _on_start_pressed() -> void:
+func _on_StartAsyncButton_pressed() -> void:
 	SceneSwitcher.change_scene("res://src/World/World.tscn")
-	#self.get_tree().change_scene("res://src/World/World.tscn")
+
+func _on_StartSyncButton_pressed() -> void:
+	# Remove the old scene
+	var start := OS.get_ticks_msec()
+	var tree = self.get_tree()
+	var old_scene = tree.current_scene
+	tree.root.remove_child(old_scene)
+	old_scene.queue_free()
+	print("Removing old scene took %sms" % [OS.get_ticks_msec() - start])
+
+	# Load the new scene
+	start = OS.get_ticks_msec()
+	var scene := ResourceLoader.load("res://src/World/World.tscn")
+	print("Loading scene took %sms" % [OS.get_ticks_msec() - start])
+
+	# Instance the new scene
+	start = OS.get_ticks_msec()
+	var instance = scene.instance()
+	print("Instancing scene took %sms" % [OS.get_ticks_msec() - start])
+
+	# Add the new scene
+	start = OS.get_ticks_msec()
+	tree.root.add_child(instance)
+	print("Adding scene took %sms" % [OS.get_ticks_msec() - start])
+
+	tree.set_current_scene(instance)
+
