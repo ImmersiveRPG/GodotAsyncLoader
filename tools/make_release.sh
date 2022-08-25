65d451c35daa5df1348dce61da4ee287549036a8
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2022-08-25 15:55:04 -0700

    Added script to make release

---
+++ tools/make_release.sh
@@ -0,0 +1,21 @@
+
+# Stop and exit on error
+set -e
+
+VERSION="0.5.0"
+
+# Change dir to this scripts directory
+script_dir=$(dirname $0)
+cd $script_dir
+
+# Generate plugin config
+cd ..
+sed 's/$VERSION/'$VERSION'/g' tools/plugin.template.cfg > addons/GodotAsyncLoader/plugin.cfg
+
+# Create release
+git commit -a -m "Release $VERSION"
+git push
+
+# Create and push tag
+git tag v$VERSION -m "Release $VERSION"
+git push --tags
