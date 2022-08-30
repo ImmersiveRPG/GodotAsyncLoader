d02d742ac73382086c25753e9b9d5084396ce100
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2022-08-30 10:02:50 -0700

    Made change_scene work with callback changes

--- addons/GodotAsyncLoader/SceneSwitcher.gd
+++ addons/GodotAsyncLoader/SceneSwitcher.gd
@@ -10,42 +10,33 @@ func change_scene(scene_path : String, loading_path := "") -> void:
 		"scene_path" : scene_path,
 	}
 
-#	# Make sure the scene exists before starting to load
-#	if not ResourceLoader.exists(scene_path):
-#		push_error("Scene files does not exist: %s" % [scene_path])
-#		return
-#
-#	# Make sure the loading scene exists before starting to load
-#	if loading_path and not ResourceLoader.exists(loading_path):
-#		push_error("Loading scene files does not exist: %s" % [loading_path])
-#		return
-#
-#	# Show the loading screen
-#	if loading_path:
-#		var err : int = get_tree().change_scene(loading_path)
-#		assert(err == OK)
+	# Make sure the scene exists before starting to load
+	if not ResourceLoader.exists(scene_path):
+		push_error("Scene files does not exist: %s" % [scene_path])
+		return
+
+	# Make sure the loading scene exists before starting to load
+	if loading_path and not ResourceLoader.exists(loading_path):
+		push_error("Loading scene files does not exist: %s" % [loading_path])
+		return
+
+	# Show the loading screen
+	if loading_path:
+		var err : int = get_tree().change_scene(loading_path)
+		assert(err == OK)
 
 	# Load the scene
 	var cb := funcref(self, "_on_scene_loaded")
 	AsyncLoader.instance_with_cb(scene_path, cb, data)
 
 func _on_scene_loaded(instance : Node, data : Dictionary) -> void:
+	# Get old current scene
 	var tree : SceneTree = self.get_tree()
-	print(["????? _on_scene_loaded", instance, data])
-	for child in tree.root.get_children():
-		print(["!!!!!!!!!!!!!!!! ", child])
+	var old_scene = tree.current_scene
+
+	# Add new scene and make it current
 	tree.root.add_child(instance)
-#	var new_scene = instance
-#	#var scene_path = data["scene_path"]
-#
-#	# Remove the old scene
-#	var old_scene = tree.current_scene
-#	tree.root.remove_child(old_scene)
-#	old_scene.queue_free()
-#
-#	# Add the new scene
-#	tree.root.add_child(instance)
-#
-#	# Change to the new scene
-#	tree.set_current_scene(instance)
-#	#tree.current_scene.queue_free()
+	tree.set_current_scene(instance)
+
+	# Free old scene
+	old_scene.queue_free()
