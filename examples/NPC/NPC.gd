4d23be1a8046cf091ad407de68c8edc1724f5d0a
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2023-01-28 10:29:06 -0800

    Added button that adds oranges async

--- examples/NPC/NPC.gd
+++ examples/NPC/NPC.gd
@@ -55,3 +55,22 @@ func _on_TimerChangeDestination_timeout() -> void:
 		Global._rng.randf_range(0.0, 0.0),
 		Global._rng.randf_range(-r, r)
 	)
+
+
+func _on_add_orange_async_pressed() -> void:
+	var r := 100.0
+	var pos := Vector3(
+		rand_range(-r, r),
+		20.0,
+		rand_range(-r, r)
+	)
+
+	# Instance scene asynchronously and send to callback
+	var target = get_tree().get_current_scene()
+	var scene_file := "res://examples/Items/Orange/Orange.tscn"
+	SceneLoader.load_scene_async_with_cb(target, scene_file, pos, true, funcref(self, "on_orange_loaded"), {})
+
+func on_orange_loaded(path : String, instance : Node, pos : Vector3, is_pos_global : bool, data : Dictionary) -> void:
+	var target = get_tree().get_current_scene()
+	instance.transform.origin = pos
+	target.add_child(instance)
