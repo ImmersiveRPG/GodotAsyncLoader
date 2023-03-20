15eb86d023e666c4c90f0fb2f061d1063d5bd506
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2023-03-19 20:55:55 -0700

    Removed unused code

--- addons/GodotAsyncLoader/Singletons/AsyncLoaderHelpers.gd
+++ addons/GodotAsyncLoader/Singletons/AsyncLoaderHelpers.gd
@@ -54,30 +54,3 @@ static func recursively_get_all_children_of_type(target : Node, target_type) -> 
 			matches.append(entry)
 
 	return matches
-
-static func call_deferred_and_return_yielded(obj : Object, method : String, args := []):
-	var fn := _CallDeferredReturnYield.new(obj, method, args)
-	var retval = fn._call()
-	#print("retval: %s" % [retval])
-	return retval
-
-class _CallDeferredReturnYield:
-	signal on_done(retval)
-	var _obj : Object
-	var _method : String
-	var _args := []
-
-	func _init(obj : Object, method : String, args := []) -> void:
-		_obj = obj
-		_method = method
-		_args = args
-
-	func _call():
-		self.call_deferred("_call_and_emit_signal_on_done", _obj, _method, _args)
-		var retval = yield(self, "on_done")
-		return retval
-
-	func _call_and_emit_signal_on_done(obj : Object, method : String, args := []) -> void:
-		#print("calling: %s, %s, %s" % [obj, method, args])
-		var retval = obj.callv(method, args)
-		self.emit_signal("on_done", retval)
